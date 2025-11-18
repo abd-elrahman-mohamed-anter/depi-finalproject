abf226fd89ddd10bbc0b35035a83757535e781cd
abd-elrahman-mohamed-anter <abdoomohamed4567@gmail.com>
2025-11-18 14:14:32 +0200

    Update main.tf

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -1,56 +1,61 @@
+##########################
+# Provider & AZs
+##########################
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "us-east-1"
 }
 
-resource "aws_vpc" "abrahimcse_vpc" {
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+##########################
+# VPC & Networking
+##########################
+resource "aws_vpc" "project_vpc" {
   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name = "abrahimcse-vpc"
-  }
+  tags = { Name = "project-vpc" }
 }
 
-resource "aws_subnet" "abrahimcse_subnet" {
-  count = 2
-  vpc_id                  = aws_vpc.abrahimcse_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.abrahimcse_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["ap-southeast-1a", "ap-southeast-1b"], count.index)
+resource "aws_subnet" "project_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.project_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.project_vpc.cidr_block, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "abrahimcse-subnet-${count.index}"
-  }
+  tags = { Name = "project-subnet-${count.index}" }
 }
 
-resource "aws_internet_gateway" "abrahimcse_igw" {
-  vpc_id = aws_vpc.abrahimcse_vpc.id
+resource "aws_internet_gateway" "project_igw" {
+  vpc_id = aws_vpc.project_vpc.id
 
-  tags = {
-    Name = "abrahimcse-igw"
-  }
+  tags = { Name = "project-igw" }
 }
 
-resource "aws_route_table" "abrahimcse_route_table" {
-  vpc_id = aws_vpc.abrahimcse_vpc.id
+resource "aws_route_table" "project_route_table" {
+  vpc_id = aws_vpc.project_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.abrahimcse_igw.id
+    gateway_id = aws_internet_gateway.project_igw.id
   }
 
-  tags = {
-    Name = "abrahimcse-route-table"
-  }
+  tags = { Name = "project-route-table" }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.abrahimcse_subnet[count.index].id
-  route_table_id = aws_route_table.abrahimcse_route_table.id
+  subnet_id      = aws_subnet.project_subnet[count.index].id
+  route_table_id = aws_route_table.project_route_table.id
 }
 
-resource "aws_security_group" "abrahimcse_cluster_sg" {
-  vpc_id = aws_vpc.abrahimcse_vpc.id
+##########################
+# Security Groups
+##########################
+resource "aws_security_group" "project_cluster_sg" {
+  vpc_id = aws_vpc.project_vpc.id
 
   egress {
     from_port   = 0
@@ -59,19 +64,26 @@ resource "aws_security_group" "abrahimcse_cluster_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "abrahimcse-cluster-sg"
-  }
+  tags = { Name = "project-cluster-sg" }
 }
 
-resource "aws_security_group" "abrahimcse_node_sg" {
-  vpc_id = aws_vpc.abrahimcse_vpc.id
+resource "aws_security_group" "project_node_sg" {
+  vpc_id = aws_vpc.project_vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH from your IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["156.221.20.201/32"]
+  }
+
+  ingress {
+    description     = "Allow communication with cluster SG"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.project_cluster_sg.id]
   }
 
   egress {
@@ -81,43 +93,14 @@ resource "aws_security_group" "abrahimcse_node_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "abrahimcse-node-sg"
-  }
+  tags = { Name = "project-node-sg" }
 }
 
-resource "aws_eks_cluster" "abrahimcse" {
-  name     = "abrahimcse-cluster"
-  role_arn = aws_iam_role.abrahimcse_cluster_role.arn
-
-  vpc_config {
-    subnet_ids         = aws_subnet.abrahimcse_subnet[*].id
-    security_group_ids = [aws_security_group.abrahimcse_cluster_sg.id]
-  }
-}
-
-resource "aws_eks_node_group" "abrahimcse" {
-  cluster_name    = aws_eks_cluster.abrahimcse.name
-  node_group_name = "abrahimcse-node-group"
-  node_role_arn   = aws_iam_role.abrahimcse_node_group_role.arn
-  subnet_ids      = aws_subnet.abrahimcse_subnet[*].id
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
-  }
-
-  instance_types = ["t2.large"]
-
-  remote_access {
-    ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.abrahimcse_node_sg.id]
-  }
-}
-
-resource "aws_iam_role" "abrahimcse_cluster_role" {
-  name = "abrahimcse-cluster-role"
+##########################
+# IAM Roles
+##########################
+resource "aws_iam_role" "project_cluster_role" {
+  name = "project-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -125,9 +108,7 @@ resource "aws_iam_role" "abrahimcse_cluster_role" {
   "Statement": [
     {
       "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
+      "Principal": { "Service": "eks.amazonaws.com" },
       "Action": "sts:AssumeRole"
     }
   ]
@@ -135,13 +116,18 @@ resource "aws_iam_role" "abrahimcse_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "abrahimcse_cluster_role_policy" {
-  role       = aws_iam_role.abrahimcse_cluster_role.name
+resource "aws_iam_role_policy_attachment" "project_cluster_role_policy" {
+  role       = aws_iam_role.project_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "abrahimcse_node_group_role" {
-  name = "abrahimcse-node-group-role"
+resource "aws_iam_role_policy_attachment" "project_cluster_service_policy" {
+  role       = aws_iam_role.project_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
+resource "aws_iam_role" "project_node_group_role" {
+  name = "project-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -149,9 +135,7 @@ resource "aws_iam_role" "abrahimcse_node_group_role" {
   "Statement": [
     {
       "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
+      "Principal": { "Service": "ec2.amazonaws.com" },
       "Action": "sts:AssumeRole"
     }
   ]
@@ -159,17 +143,61 @@ resource "aws_iam_role" "abrahimcse_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "abrahimcse_node_group_role_policy" {
-  role       = aws_iam_role.abrahimcse_node_group_role.name
+resource "aws_iam_role_policy_attachment" "project_node_group_role_policy" {
+  role       = aws_iam_role.project_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "abrahimcse_node_group_cni_policy" {
-  role       = aws_iam_role.abrahimcse_node_group_role.name
+resource "aws_iam_role_policy_attachment" "project_node_group_cni_policy" {
+  role       = aws_iam_role.project_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "abrahimcse_node_group_registry_policy" {
-  role       = aws_iam_role.abrahimcse_node_group_role.name
+resource "aws_iam_role_policy_attachment" "project_node_group_registry_policy" {
+  role       = aws_iam_role.project_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+##########################
+# EKS Cluster & Node Group
+##########################
+resource "aws_eks_cluster" "project" {
+  name     = "project-cluster"
+  role_arn = aws_iam_role.project_cluster_role.arn
+
+  vpc_config {
+    subnet_ids         = aws_subnet.project_subnet[*].id
+    security_group_ids = [aws_security_group.project_cluster_sg.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.project_cluster_role_policy,
+    aws_iam_role_policy_attachment.project_cluster_service_policy
+  ]
+}
+
+resource "aws_eks_node_group" "project" {
+  cluster_name    = aws_eks_cluster.project.name
+  node_group_name = "project-node-group"
+  node_role_arn   = aws_iam_role.project_node_group_role.arn
+  subnet_ids      = aws_subnet.project_subnet[*].id
+
+  scaling_config {
+    desired_size = 5
+    max_size     = 5
+    min_size     = 3
+  }
+
+  instance_types = ["t3.micro"]
+
+  remote_access {
+    ec2_ssh_key               = var.ssh_key_name
+    source_security_group_ids = [aws_security_group.project_node_sg.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.project_node_group_role_policy,
+    aws_iam_role_policy_attachment.project_node_group_cni_policy,
+    aws_iam_role_policy_attachment.project_node_group_registry_policy
+  ]
 }
